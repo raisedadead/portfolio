b5d2f7520a5c2e01a105e1a2a83068ce8a210390
Mrugesh Mohapatra <me@raisedadead.com>
2017-11-05 14:16:06 +0530

    feat: Update basic content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="wrapper">
+        <div id="bg" />
+        <div id="overlay" />
+        <div id="main">
+          <header id="header">
+            <h1>mrugesh mohapatra</h1>
+            <p>
+              developer. music addict. open source enthusiast. noob
+              photographer.
+            </p>
+            <nav>
+              <ul>
+                <li>
+                  <a href="https://twitter.com/raisedadead">
+                    <span class="label">Twitter</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://freeCodeCamp.org/raisedadead">
+                    <span class="label">freeCodeCamp</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/raisedadead">
+                    <span class="label">GitHub</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://blog.raisedadead.com">
+                    <span class="label">Medium</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://linkedin.com/in/mrugeshm">
+                    <span class="label">LinkedIn</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <footer id="footer">
+            <span class="copyright">
+              COPYRIGHT &copy; 2017 MRUGESH MOHAPATRA.
+            </span>
+          </footer>
+        </div>
       </div>
     );
   }
