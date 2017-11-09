5013bacd5d9446739d0dd670bdc28a99397ebacd
Mrugesh Mohapatra <me@raisedadead.com>
2017-11-10 00:38:10 +0530

    fix(nav): Refactored social nav as functional component

--- src/components/Nav/SocialNav.js
+++ src/components/Nav/SocialNav.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class SocialNav extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li>
-            <a href="https://twitter.com/raisedadead">
-              <span className="label">Twitter</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://freeCodeCamp.org/raisedadead">
-              <span className="label">freeCodeCamp</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/raisedadead">
-              <span className="label">GitHub</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://blog.raisedadead.com">
-              <span className="label">Medium</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/mrugeshm">
-              <span className="label">LinkedIn</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    )
-  }
+const SocialNav = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="https://twitter.com/raisedadead">
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://freeCodeCamp.org/raisedadead">
+            <span className="label">freeCodeCamp</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/raisedadead">
+            <span className="label">GitHub</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://blog.raisedadead.com">
+            <span className="label">Medium</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/mrugeshm">
+            <span className="label">LinkedIn</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
+
+export default SocialNav
