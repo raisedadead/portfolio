5d5392d9982190dee2e635c8f1d7025c5938e3e8
Mrugesh Mohapatra <me@raisedadead.com>
2017-11-10 11:56:39 +0530

    feat: Testing centering layout

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { TopNav, SocialNav, Footer } from './components'
 export default class App extends Component {
   render() {
     return (
-      <div className="uk-container">
+      <div className="uk-container uk-height-viewport uk-background-muted">
         <TopNav />
-        <div>
+        <div className="uk-position-center uk-text-center">
           <h1 className="uk-heading-primary">mrugesh mohapatra</h1>
           <p>
             developer. music addict. open source enthusiast. noob photographer.
           </p>
+          <SocialNav />
+          <Footer />
         </div>
-        <SocialNav />
-        <Footer />
       </div>
     )
   }
--- src/components/Nav/SocialNav.js
+++ src/components/Nav/SocialNav.js
@@ -3,70 +3,72 @@ import React from 'react'
 const raisedButtonStyle = { height: 50, margin: '4px 0 4px 0' }
 const SocialNav = () => {
   return (
-    <div>
-      <ul className="uk-navbar-nav uk-iconnav">
-        <li>
-          <a
-            title="Twitter"
-            href="https://twitter.com/raisedadead"
-            className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
-            data-uk-icon="icon: twitter"
-            data-uk-tooltip="pos: bottom"
-            style={raisedButtonStyle}
-          >
-            <span className="label uk-hidden">Twitter</span>
-          </a>
-        </li>
-        <li>
-          <a
-            title="GitHub"
-            href="https://github.com/raisedadead"
-            className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
-            data-uk-icon="icon: github"
-            data-uk-tooltip="pos: bottom"
-            style={raisedButtonStyle}
-          >
-            <span className="label uk-hidden">GitHub</span>
-          </a>
-        </li>
-        <li>
-          <a
-            title="Medium"
-            href="https://blog.raisedadead.com"
-            className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
-            data-uk-icon="icon: world"
-            data-uk-tooltip="pos: bottom"
-            style={raisedButtonStyle}
-          >
-            <span className="label uk-hidden">Medium</span>
-          </a>
-        </li>
-        <li>
-          <a
-            title="freeCodeCamp"
-            href="https://freeCodeCamp.org/raisedadead"
-            className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
-            data-uk-icon="icon: code"
-            data-uk-tooltip="pos: bottom"
-            style={raisedButtonStyle}
-          >
-            <span className="label uk-hidden">freeCodeCamp</span>
-          </a>
-        </li>
-        <li>
-          <a
-            title="LinkedIn"
-            href="https://linkedin.com/in/mrugeshm"
-            className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
-            data-uk-icon="icon: linkedin"
-            data-uk-tooltip="pos: bottom"
-            style={raisedButtonStyle}
-          >
-            <span className="label uk-hidden">LinkedIn</span>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <nav className="uk-navbar-container uk-navbar-transparent" data-uk-navbar>
+      <div className="uk-navbar-center">
+        <ul className="uk-navbar-nav uk-iconnav">
+          <li>
+            <a
+              title="Twitter"
+              href="https://twitter.com/raisedadead"
+              className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
+              data-uk-icon="icon: twitter"
+              data-uk-tooltip="pos: bottom"
+              style={raisedButtonStyle}
+            >
+              <span className="label uk-hidden">Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a
+              title="GitHub"
+              href="https://github.com/raisedadead"
+              className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
+              data-uk-icon="icon: github"
+              data-uk-tooltip="pos: bottom"
+              style={raisedButtonStyle}
+            >
+              <span className="label uk-hidden">GitHub</span>
+            </a>
+          </li>
+          <li>
+            <a
+              title="Medium"
+              href="https://blog.raisedadead.com"
+              className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
+              data-uk-icon="icon: world"
+              data-uk-tooltip="pos: bottom"
+              style={raisedButtonStyle}
+            >
+              <span className="label uk-hidden">Medium</span>
+            </a>
+          </li>
+          <li>
+            <a
+              title="freeCodeCamp"
+              href="https://freeCodeCamp.org/raisedadead"
+              className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
+              data-uk-icon="icon: code"
+              data-uk-tooltip="pos: bottom"
+              style={raisedButtonStyle}
+            >
+              <span className="label uk-hidden">freeCodeCamp</span>
+            </a>
+          </li>
+          <li>
+            <a
+              title="LinkedIn"
+              href="https://linkedin.com/in/mrugeshm"
+              className="uk-box-shadow-small uk-box-shadow-hover-large uk-border-circle"
+              data-uk-icon="icon: linkedin"
+              data-uk-tooltip="pos: bottom"
+              style={raisedButtonStyle}
+            >
+              <span className="label uk-hidden">LinkedIn</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
