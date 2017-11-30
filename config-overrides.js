faf6c88931b1bae3ce56379d1db8d1b8d5c1c415
Mrugesh Mohapatra <me@raisedadead.com>
2017-11-30 15:44:33 +0530

    feat(tools): add styled-components

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,6 @@
+const rewireStyledComponents = require('react-app-rewire-styled-components')
+
 module.exports = (config, env) => {
+  config = rewireStyledComponents(config, env)
   return config
 }
