29a01c644ee71d7c01b3caeae3d199f5336f4583
Mrugesh Mohapatra <me@raisedadead.com>
2017-12-28 11:14:33 +0530

    refactor(deploy): prepare for deploy on master

--- src/components/Nav/TopNav.js
+++ src/components/Nav/TopNav.js
@@ -11,10 +11,12 @@ export default () => (
         <li>
           <a
             title="résumé"
-            href="/resume"
+            href="https://git.raisedadead.com/resume/"
             className="uk-background-muted uk-box-shadow-small uk-box-shadow-hover-xlarge uk-border-rounded"
             data-uk-icon="icon: copy"
             data-uk-tooltip="pos: bottom"
+            target="_blank"
+            rel="noopener noreferrer"
             style={raisedButtonStyle}
           >
             <span className="uk-hidden">résumé</span>
@@ -32,18 +34,6 @@ export default () => (
             style={raisedButtonStyle}
           >
             <span className="uk-hidden">ask me anything</span>
-          </a>
-        </li>
-        <li>
-          <a
-            title="contact"
-            href="/contact"
-            className="uk-background-muted uk-box-shadow-small uk-box-shadow-hover-xlarge uk-border-rounded"
-            data-uk-icon="icon: mail"
-            data-uk-tooltip="pos: bottom"
-            style={raisedButtonStyle}
-          >
-            <span className="uk-hidden">Contact</span>
           </a>
         </li>
       </ul>
