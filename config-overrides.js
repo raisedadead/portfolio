d273104a1c0f51234f947a0ecb254c5b7d8f59b3
mrugesh mohapatra <1884376+raisedadead@users.noreply.github.com>
2018-12-07 02:16:31 +0530

    chore(tools): cleanup old dependenices (#86)

--- config-overrides.js
+++
@@ -1,6 +0,0 @@
-const rewireStyledComponents = require('react-app-rewire-styled-components')
-
-module.exports = (config, env) => {
-  config = rewireStyledComponents(config, env)
-  return config
-}
