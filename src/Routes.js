275f63eaee421f9cf872ac4c2fdf9554c0b94a12
Mrugesh Mohapatra <me@raisedadead.com>
2019-01-02 19:11:43 +0530

    fix(GA): update to a react component

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
+import withTracker from './withTracker'
 import { Home, Resume, Contact, NotFound } from './containers'
 
 export default () => (
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/resume" exact component={Resume} />
-    <Route path="/contact" exact component={Contact} />
+    <Route path="/" exact component={withTracker(Home)} />
+    <Route path="/resume" exact component={withTracker(Resume)} />
+    <Route path="/contact" exact component={withTracker(Contact)} />
     <Route component={NotFound} />
   </Switch>
 )
