5d766034f13d6d00efc28d6a97d2638a50d227c0
Mrugesh Mohapatra <me@raisedadead.com>
2019-01-03 21:37:52 +0530

    fix(home): wrap content in tags for a11y

--- src/containers/HomeContent.js
+++ src/containers/HomeContent.js
@@ -6,24 +6,28 @@ const tagline = StylesHelper('uk-text-large')
 
 export default props => (
   <div {...props}>
-    <p
-      className={heading}
-      style={{
-        color: '#F5F5F5',
-        fontFamily: "'Quicksand', sans-serif",
-        fontWeight: 'bold'
-      }}
-    >
-      mrugesh mohapatra
-    </p>
-    <p
-      className={tagline}
-      style={{ color: '#D5F7FF', fontFamily: "'Quicksand', sans-serif" }}
-    >
-      developer. music addict. open source enthusiast. noob photographer.
-    </p>
-    <p style={{ color: '#D5F7FF', fontFamily: "'Quicksand', sans-serif" }}>
-      Developer Advocate at freeCodeCamp.org
-    </p>
+    <header>
+      <h1
+        className={heading}
+        style={{
+          color: '#F5F5F5',
+          fontFamily: "'Quicksand', sans-serif",
+          fontWeight: 'bold'
+        }}
+      >
+        mrugesh mohapatra
+      </h1>
+    </header>
+    <main>
+      <h2
+        className={tagline}
+        style={{ color: '#D5F7FF', fontFamily: "'Quicksand', sans-serif" }}
+      >
+        developer. music addict. open source enthusiast. noob photographer.
+      </h2>
+      <p style={{ color: '#D5F7FF', fontFamily: "'Quicksand', sans-serif" }}>
+        Developer Advocate at freeCodeCamp.org
+      </p>
+    </main>
   </div>
 )
