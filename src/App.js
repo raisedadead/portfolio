cb6c6435c45895586ab0739e769a94c0de901438
Mrugesh Mohapatra <me@raisedadead.com>
2019-01-06 22:14:54 +0530

    feat: add a loading progress

--- src/App.js
+++ src/App.js
@@ -3,7 +3,37 @@ import Routes from './Routes'
 import { PageHeaders } from './components'
 
 export default class App extends Component {
+  state = {
+    loading: true,
+    loadStartTime: new Date()
+  }
+
+  componentDidMount() {
+    const loadTime = new Date() - this.state.loadStartTime
+    if (loadTime < 200) {
+      setTimeout(() => {
+        this.setState({ loading: false })
+      }, 1000)
+    } else {
+      this.setState({ loading: false })
+    }
+  }
+
   render() {
+    const { loading } = this.state
+
+    if (loading) {
+      return null
+    }
+
+    const spinnerContainer = document.getElementById('spinner-container')
+    if (spinnerContainer) {
+      spinnerContainer.classList.add('available')
+      setTimeout(() => {
+        // remove from DOM
+        spinnerContainer.outerHTML = ''
+      }, 2000)
+    }
     return (
       <div className="Application">
         <PageHeaders />
--- src/components/Utils/ImageLoader.js
+++ src/components/Utils/ImageLoader.js
@@ -8,7 +8,7 @@ export default class ImageLoader extends Component {
   }
 
   onImageLoad = () => {
-    const updatedStyle = { opacity: 1 }
+    const updatedStyle = { opacity: 1, transition: 'opacity, 1.5s' }
     this.setState({ style: updatedStyle })
   }
 
