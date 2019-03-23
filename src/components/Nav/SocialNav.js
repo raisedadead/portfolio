bca35e58ef2ef04247a4e79410e1ea88d48b624d
Mrugesh Mohapatra <me@raisedadead.com>
2019-03-24 03:23:37 +0530

    fix(perf): add a11y complaint labels to links

--- src/components/Nav/SocialNav.js
+++ src/components/Nav/SocialNav.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import { PrimaryNav, NavItem } from 'mineral-ui/Navigation';
-import Tooltip from 'mineral-ui/Tooltip';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import LinkComponent from '../LinkComponent';
 
 const SocialNav = () => (
   <PrimaryNav itemAs={LinkComponent} minimal alignment="center">
-    <NavItem to="https://twitter.com/raisedadead">
-      <Tooltip content="Twitter">
-        <FontAwesomeIcon icon={[`fab`, `twitter`]} size="lg" />
-      </Tooltip>
+    <NavItem to="https://twitter.com/raisedadead" aria-label="Twitter">
+      <FontAwesomeIcon icon={[`fab`, `twitter`]} size="lg" />
     </NavItem>
-    <NavItem to="https://github.com/raisedadead">
-      <Tooltip content="GitHub">
-        <FontAwesomeIcon icon={[`fab`, `github`]} size="lg" />
-      </Tooltip>
+    <NavItem to="https://github.com/raisedadead" aria-label="GitHub">
+      <FontAwesomeIcon icon={[`fab`, `github`]} size="lg" />
     </NavItem>
-    <NavItem to="https://blog.raisedadead.com">
-      <Tooltip content="Medium">
-        <FontAwesomeIcon icon={[`fab`, `medium`]} size="lg" />
-      </Tooltip>
+    <NavItem to="https://blog.raisedadead.com" aria-label="Medium">
+      <FontAwesomeIcon icon={[`fab`, `medium`]} size="lg" />
     </NavItem>
-    <NavItem to="https://freeCodeCamp.org/raisedadead">
-      <Tooltip content="freeCodeCamp">
-        <FontAwesomeIcon icon={[`fab`, `free-code-camp`]} size="lg" />
-      </Tooltip>
+    <NavItem
+      to="https://freeCodeCamp.org/raisedadead"
+      aria-label="freeCodeCamp.org"
+    >
+      <FontAwesomeIcon icon={[`fab`, `free-code-camp`]} size="lg" />
     </NavItem>
-    <NavItem to="https://linkedin.com/in/mrugeshm">
-      <Tooltip content="LinkedIn">
-        <FontAwesomeIcon icon={[`fab`, `linkedin`]} size="lg" />
-      </Tooltip>
+    <NavItem to="https://linkedin.com/in/mrugeshm" aria-label="LinkedIn">
+      <FontAwesomeIcon icon={[`fab`, `linkedin`]} size="lg" />
     </NavItem>
   </PrimaryNav>
 );
