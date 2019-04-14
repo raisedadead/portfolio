f40c565318118393a154ca37d95349ccd0c936d5
Mrugesh Mohapatra <me@raisedadead.com>
2019-04-15 02:22:09 +0530

    fix(nav): update main nave styles

--- src/components/Nav/MainNav.js
+++ src/components/Nav/MainNav.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { PrimaryNav, NavItem } from 'mineral-ui/Navigation';
+import styled from '@emotion/styled';
+import { PrimaryNav, NavItem as NavigationItem } from 'mineral-ui/Navigation';
 import Link from 'mineral-ui/Link';
 import Box from 'mineral-ui/Box/Box';
 
+const NavItem = styled(NavigationItem)({
+  minWidth: `auto`,
+  minHeight: `auto`,
+  padding: `0.5em 0.5em`,
+  height: `auto`
+});
+
 const MainNaV = () => (
   <Box marginHorizontal="auto" width={1 / 2}>
-    <PrimaryNav itemAs={Link} align="end" minimal>
+    <PrimaryNav
+      css={{
+        width: `75vw`
+      }}
+      itemAs={Link}
+      align="end"
+      minimal
+    >
       {/*
       <NavItem to="https://git.raisedadead.com/resume/">
         <span>résumé</span>
