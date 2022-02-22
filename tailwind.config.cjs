77f32a6b002eb13532f26577d858e974a740c48e
Mrugesh Mohapatra <hi@mrugesh.dev>
2022-02-22 17:57:37 +0530

    feat: update templates and dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,48 +1,14 @@
-const { tailwindExtractor } = require('tailwindcss/lib/lib/purgeUnusedStyles');
-const theme = require('tailwindcss/defaultTheme');
-const colors = require('tailwindcss/colors');
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms');
 
-module.exports = {
-  purge: {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-    options: {
-      defaultExtractor: (content) => [
-        // If this stops working, please open an issue at https://github.com/svelte-add/tailwindcss/issues rather than bothering Tailwind Labs about it
-        ...tailwindExtractor(content),
-        // Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
-        ...[...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(
-          ([_match, group, ..._rest]) => group
-        )
-      ],
-      keyframes: true
-    }
-  },
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
   theme: {
-    extend: {
-      fontFamily: {
-        sans: [...theme.fontFamily.sans]
-      }
-    },
-    boxShadow: {
-      ...theme.boxShadow,
-      card: '50px 50px 100px #000'
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      ...colors,
-      gray: colors.blueGray,
-      red: colors.rose,
-      yellow: colors.yellow,
-      green: colors.emerald,
-      blue: colors.teal,
-      indigo: colors.violet,
-      purple: colors.fuchsia,
-      pink: colors.pink
-    }
-  },
-  variants: {
     extend: {}
   },
-  plugins: []
+
+  plugins: [forms, typography]
 };
+
+module.exports = config;
