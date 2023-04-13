cfe59ae704c2a5decbfc864e493b5c709e4a10e1
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-04-13 15:47:27 +0530

    feat: create a uses page

---
+++ src/pages/uses.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layouts';
+
+const Uses: NextPage = () => {
+  return (
+    <Layout>
+      <section>
+        <div className='prose prose-slate pb-5 pt-5'>
+          <h1 className='text-2xl font-bold text-slate-700'>Uses</h1>
+          <p>Coming soon...</p>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+export default Uses;
