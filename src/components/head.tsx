508475481b1e0aeb33254a1554c2fcfa47b8a212
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-05-07 22:08:57 +0530

    fix: og image path and GTM

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -22,7 +22,10 @@ export const MetaHead = () => {
           itemProp='description'
           content='Namaste! I am a software engineer based out of Bengaluru, India. I am passionate about Aviation, Open Source, Education for All, and Site Reliability Engineering.'
         />
-        <meta itemProp='image' content='http://mrugesh.dev/og-image.webp' />
+        <meta
+          itemProp='image'
+          content='http://mrugesh.dev/images/og-image.webp'
+        />
         {/* Facebook Meta Tags */}
         <meta property='og:url' content='https://mrugesh.dev' />
         <meta property='og:type' content='website' />
@@ -34,7 +37,10 @@ export const MetaHead = () => {
           property='og:description'
           content='Namaste! I am a software engineer based out of Bengaluru, India. I am passionate about Aviation, Open Source, Education for All, and Site Reliability Engineering.'
         />
-        <meta property='og:image' content='http://mrugesh.dev/og-image.webp' />
+        <meta
+          property='og:image'
+          content='http://mrugesh.dev/images/og-image.webp'
+        />
         {/* Twitter Meta Tags */}
         <meta name='twitter:card' content='summary_large_image' />
         <meta
@@ -45,7 +51,10 @@ export const MetaHead = () => {
           name='twitter:description'
           content='Namaste! I am a software engineer based out of Bengaluru, India. I am passionate about Aviation, Open Source, Education for All, and Site Reliability Engineering.'
         />
-        <meta name='twitter:image' content='http://mrugesh.dev/og-image.webp' />
+        <meta
+          name='twitter:image'
+          content='http://mrugesh.dev/images/og-image.webp'
+        />
       </Head>
       <Script
         id='google-analytics'
