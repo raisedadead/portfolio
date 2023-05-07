5184c9010239541f49bac28c7530520e0249e913
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-05-07 21:31:03 +0530

    feat: new navigation menu

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -18,17 +18,19 @@ export const Layout: React.FC<LayoutProps> = ({
         <Background />
       </div>
 
-      <Nav className='fixed top-0 z-20 mx-auto max-h-none w-screen' />
+      <Nav className='fixed right-0 top-0 z-20 mx-auto max-h-none w-screen' />
 
       <main
         className={`relative z-10 mx-auto mb-10 mt-16 h-auto max-h-none w-[90%] overflow-y-auto pb-10 pt-5 lg:w-[75%] xl:w-[60%] ${
-          showGlass ? ' rounded bg-white/30 backdrop-blur-lg' : ''
+          showGlass
+            ? ' rounded bg-white/30 shadow-[4px_4px_0_0_rgba(60,64,43,.2)] backdrop-blur-lg'
+            : ''
         }`}
       >
         <div className='px-5 pb-5 pt-5 md:px-20 '>{children}</div>
         <Footer isDefault={true} className='p-2' />
       </main>
-      <ScrollButton className='fixed bottom-8 right-10 z-20 md:right-20 lg:right-48' />
+      <ScrollButton className='fixed bottom-6 right-8 z-20 md:right-20 lg:right-48' />
     </>
   );
 };
