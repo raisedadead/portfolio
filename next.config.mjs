145c3d33886438ad7ab0083bfce20bab95ed434e
Mrugesh Mohapatra <noreply@mrugesh.dev>
2023-06-03 15:58:39 +0530

    feat: add redirects for /meet

--- next.config.mjs
+++ next.config.mjs
@@ -19,5 +19,28 @@ export default defineNextConfig({
   swcMinify: true,
   experimental: {
     nextScriptWorkers: true
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/meet',
+        destination: 'https://topmate.io/mrugesh',
+        basePath: false,
+        permanent: false
+      },
+      {
+        source: '/links',
+        destination: 'https://bio.link/mrugesh',
+        basePath: false,
+        permanent: false
+      },
+      {
+        source: '/meet/fcc',
+        destination:
+          'https://calendar.google.com/calendar/appointments/schedules/AcZssZ0sgE0ZnfSxZQ9fc8fNMW_3bGoA9im9vJ-0ADu_dcMAkvt4GJxVMhzM7IkclZ-6NV1g7KTNrOxa',
+        basePath: false,
+        permanent: false
+      }
+    ];
   }
 });
