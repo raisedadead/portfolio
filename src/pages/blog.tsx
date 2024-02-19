569c7cd8494e13a39207719553670a402906842f
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-02-19 15:05:42 +0530

    chore: update buttons

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -136,7 +136,7 @@ const Blog: NextPage = () => {
       <div className='flex justify-center py-5'>
         <button
           onClick={loadMoreArticles}
-          className='items-center rounded bg-orange-100 px-4 py-2 text-sm font-medium text-black shadow-[4px_4px_0_0_rgba(60,64,43,.2)] backdrop-blur-sm hover:bg-orange-300 hover:text-slate-800 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:opacity-50 disabled:hover:bg-gray-100 disabled:hover:text-black'
+          className='items-center rounded border-b-2 border-r-2 border-gray-600 bg-orange-100 px-4 py-2 text-sm font-medium text-black shadow-[4px_4px_0_0_rgba(60,64,43,.2)] backdrop-blur-sm hover:bg-orange-300 hover:text-slate-800 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-gray-100 disabled:opacity-50 disabled:hover:bg-gray-100 disabled:hover:text-black'
           disabled={!hasNextPage || isLoadingMore}
         >
           {isLoadingMore ? (
