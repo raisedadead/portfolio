6d746211b039015670cb7ec58e0b470e91523194
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-03-08 16:30:31 +0530

    feat: add more transparency

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -22,11 +22,11 @@ export const Layout: React.FC<LayoutProps> = ({
 
       <main
         className={`relative z-10 mx-auto mb-8 mt-8 h-auto max-h-none w-[90%] pb-8 pt-5 lg:w-[75%] xl:w-[60%] ${
-          showGlass ? '  bg-teal-100/80 shadow-[8px_6px_0px_rgba(0,0,0,1)]' : ''
+          showGlass ? '  bg-white/50 shadow-[8px_6px_0px_rgba(0,0,0,1)]' : ''
         }`}
       >
         <Nav
-          className='mb-2 max-h-none w-screen'
+          className='mb-6 max-h-none w-screen'
           showHomeButton={showHomeButton}
         />
         <div className='px-5 pb-5 pt-5 md:px-16'>{children}</div>
