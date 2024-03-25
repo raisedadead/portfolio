048f04526fb01e3a17feaed4a7b1d2a30accdff2
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-03-25 15:55:28 +0530

    feat: experiment with fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import type { AppType } from 'next/dist/shared/lib/utils';
 import { GoogleTagManager } from '@next/third-parties/google';
+import * as Fonts from '@/components/fonts';
 import '../styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${Fonts.IBMPlexSans.style.fontFamily};
+        }
+
+        code {
+          font-family: ${Fonts.IBMPlexMono.style.fontFamily};
+        }
+
+        p {
+          font-family: ${Fonts.IBMPlexMono.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
       <GoogleTagManager gtmId='GTM-W539SFX' />
     </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-ibm-plex-sans)'],
+        mono: ['var(--font-ibm-plex-mono)'],
+        'mono-italic': ['var(--font-ibm-plex-mono-italic)']
+      }
+    }
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
 };
