9081737fcb27c772de20956c0e2d28f5d5780af5
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-06-12 13:42:35 +0530

    fix: update formatting and tests

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -23,9 +23,7 @@ export const Layout: React.FC<LayoutProps> = ({
       </div>
 
       <main
-        className={`relative z-10 mx-auto mb-8 mt-8 h-auto max-h-none w-[90%] pb-8 pt-5
-        ${isLegalPage ? '' : 'lg:w-[75%] xl:w-[60%]'}
-        ${showGlass ? '  bg-white/50 shadow-[8px_6px_0px_rgba(0,0,0,1)]' : ''}`}
+        className={`relative z-10 mx-auto mb-8 mt-8 h-auto max-h-none w-[90%] pb-8 pt-5 ${isLegalPage ? '' : 'lg:w-[75%] xl:w-[60%]'} ${showGlass ? 'bg-white/50 shadow-[8px_6px_0px_rgba(0,0,0,1)]' : ''}`}
       >
         <Nav
           className='mb-6 max-h-none w-screen'
