f41be4205e505dae81777cb200e0613e1831d6d3
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-07-02 12:43:36 +0530

    chore: use cloudflare web-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppType } from 'next/dist/shared/lib/utils';
 import { GoogleTagManager } from '@next/third-parties/google';
+import Script from 'next/script';
 import * as Fonts from '@/components/fonts';
 import '../styles/globals.css';
 
@@ -21,6 +22,10 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       `}</style>
       <Component {...pageProps} />
       <GoogleTagManager gtmId='GTM-W539SFX' />
+      <Script
+        src='https://static.cloudflareinsights.com/beacon.min.js'
+        data-cf-beacon='{"token": "9d84395d99af4272b20ef8e6c9e19ecf"}'
+      />
     </>
   );
 };
