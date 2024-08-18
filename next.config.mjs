57dbdcf8640c8f4853bea622edf7908a2abfa9ea
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-08-19 00:13:26 +0530

    refactor: remove compose plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,27 @@
 // @ts-check
-/**
- * @type {import('next').NextConfig}
- */
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { env } from './src/env/server.mjs';
 
-import { PHASE_PRODUCTION_BUILD } from 'next/constants.js';
-import { Config } from 'next-recompose-plugins';
 import { withSentryConfig } from '@sentry/nextjs';
 import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const config = new Config(() => ({
+const isProductionBuild = process.env.NODE_ENV === 'production';
+const shouldOpenAnalyzer = process.env.OPEN_ANALYZER === 'true';
+
+/**
+ * Don't be scared of the generics here.
+ * All they do is to give us autocompletion when using this.
+ *
+ * @template {import('next').NextConfig} T
+ * @param {T} config - A generic parameter that flows through to the return type
+ * @constraint {{import('next').NextConfig}}
+ */
+function defineNextConfig(config) {
+  return config;
+}
+
+const nextConfig = defineNextConfig({
   i18n: {
     locales: ['en'],
     defaultLocale: 'en'
@@ -26,75 +36,71 @@ const config = new Config(() => ({
       }
     ]
   },
-  redirects: async () => [
-    {
-      source: '/ama',
-      destination: 'https://github.com/raisedadead/ama/discussions/14',
-      basePath: false,
-      permanent: false
-    },
-    {
-      source: '/contact',
-      destination: '/about',
-      permanent: false
-    },
-    {
-      source: '/dotfiles',
-      destination: 'https://github.com/raisedadead/dotfiles',
-      permanent: false
-    },
-    {
-      source: '/links',
-      destination: 'https://bio.link/mrugesh',
-      basePath: false,
-      permanent: false
-    },
-    {
-      source: '/meet',
-      destination: 'https://topmate.io/mrugesh',
-      basePath: false,
-      permanent: false
-    },
-    {
-      source: '/meet/personal',
-      destination: 'https://cal.com/mrugesh/meet?duration=30&layout=month_view',
-      basePath: false,
-      permanent: false
-    },
-    {
-      source: '/meet/freecodecamp',
-      destination: 'https://calendar.app.google/AfQByQkijweg98736',
-      basePath: false,
-      permanent: false
-    }
-  ]
-}))
-  .applyPlugin(
-    (phase, _args, config) =>
-      withBundleAnalyzer({ enabled: phase === PHASE_PRODUCTION_BUILD })(config),
-    '@next/bundle-analyzer'
-  )
-  .applyPlugin((_phase, _args, config) => {
-    let newConfig = withSentryConfig(config, {
-      org: 'mrugesh',
-      telemetry: false,
-      project: 'portfolio-nextjs',
-      silent: !process.env.CI,
-      widenClientFileUpload: true,
-      reactComponentAnnotation: { enabled: true },
-      tunnelRoute: '/monitoring',
-      hideSourceMaps: true,
-      disableLogger: true,
-      automaticVercelMonitors: true,
-      authToken: process.env.SENTRY_AUTH_TOKEN
-    });
+  redirects: async () => {
+    return [
+      {
+        source: '/ama',
+        destination: 'https://github.com/raisedadead/ama/discussions/14',
+        basePath: false,
+        permanent: false
+      },
+      {
+        source: '/contact',
+        destination: '/about',
+        permanent: false
+      },
+      {
+        source: '/dotfiles',
+        destination: 'https://github.com/raisedadead/dotfiles',
+        permanent: false
+      },
+      {
+        source: '/links',
+        destination: 'https://bio.link/mrugesh',
+        basePath: false,
+        permanent: false
+      },
+      {
+        source: '/meet',
+        destination: 'https://topmate.io/mrugesh',
+        basePath: false,
+        permanent: false
+      },
+      {
+        source: '/meet/personal',
+        destination:
+          'https://cal.com/mrugesh/meet?duration=30&layout=month_view',
+        basePath: false,
+        permanent: false
+      },
+      {
+        source: '/meet/freecodecamp',
+        destination: 'https://calendar.app.google/AfQByQkijweg98736',
+        basePath: false,
+        permanent: false
+      }
+    ];
+  }
+});
 
-    if (newConfig && typeof newConfig === 'object') {
-      return newConfig;
-    } else {
-      throw new Error('Invalid configuration returned by withSentryConfig');
-    }
-  }, '@sentry/nextjs')
-  .build();
+const configSentry = withSentryConfig(nextConfig, {
+  org: 'mrugesh',
+  telemetry: false,
+  project: 'portfolio-nextjs',
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: { enabled: true },
+  tunnelRoute: '/monitoring',
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+  authToken: process.env.SENTRY_AUTH_TOKEN
+});
 
+const configAnalyzerAndSentry = withBundleAnalyzer({
+  enabled: isProductionBuild,
+  openAnalyzer: shouldOpenAnalyzer
+})(configSentry);
+
+const config = isProductionBuild ? configAnalyzerAndSentry : configSentry;
 export default config;
