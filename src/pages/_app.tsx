e458cc0c5b443f8189380d7de2a581a4e816e187
Mrugesh Mohapatra <1884376+raisedadead@users.noreply.github.com>
2024-08-18 14:09:51 +0530

    fix: fonts should load earlier (#2251)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import type { AppType } from 'next/dist/shared/lib/utils';
 import { GoogleTagManager } from '@next/third-parties/google';
 import Script from 'next/script';
-
 import '@/styles/globals.css';
+import { fontSans, fontMono } from '@/components/fonts';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${fontSans.style.fontFamily};
+        }
+        code {
+          font-family: ${fontMono.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
       <GoogleTagManager gtmId='GTM-W539SFX' />
       <Script
