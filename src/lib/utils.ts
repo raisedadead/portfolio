60fb8db6f413ade44412675153423d95de5c9d94
Mrugesh Mohapatra <noreply@mrugesh.dev>
2024-09-21 02:02:27 +0530

    refactor: utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,16 +2,11 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import type { DOMNode, Element } from 'html-react-parser';
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 export const SENTRY_DSN =
   'https://e83b9674c3a509c1ef068ddd63d3adfd@o4507798912958464.ingest.us.sentry.io/4507798914859008';
-
 export const GA_TRACKING_ID = 'G-VD9T0P1KLN';
 
-export function updateGAConsent() {
+export const updateGAConsent = () => {
   // @ts-expect-error: gtag is added by Google Analytics script
   window.gtag('consent', 'update', {
     ad_user_data: 'granted',
@@ -19,14 +14,18 @@ export function updateGAConsent() {
     ad_storage: 'granted',
     analytics_storage: 'granted'
   });
-}
+};
 
-export function loadGAScript() {
+export const loadGAScript = () => {
   const script = document.createElement('script');
   script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`;
   script.async = true;
   document.head.appendChild(script);
-}
+};
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
 
 const isElement = (node: DOMNode): node is Element => {
   return node.type === 'tag';
