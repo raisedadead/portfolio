d84552a5472a9c006aa3bc056ae8f708e5fc52ac
Mrugesh Mohapatra <noreply@mrugesh.dev>
2025-01-10 00:29:12 +0530

    feat: update robots.txt config

--- sitemap.config.mjs
+++ sitemap.config.mjs
@@ -3,7 +3,24 @@ const nextSitemapConfig = {
   siteUrl: 'https://mrugesh.dev',
   generateRobotsTxt: true,
   sitemapSize: 1000,
-  exclude: ['/contact', '/404', '/privacy', '/terms', '/refunds', '/about']
+  exclude: ['/contact', '/404', '/privacy', '/terms', '/refunds', '/about'],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      {
+        userAgent: '*',
+        disallow: [
+          '/contact',
+          '/404',
+          '/privacy',
+          '/terms',
+          '/refunds',
+          '/about'
+        ]
+      }
+    ],
+    additionalSitemaps: ['https://hn.mrugesh.dev/sitemap.xml']
+  }
 };
 
 export default nextSitemapConfig;
