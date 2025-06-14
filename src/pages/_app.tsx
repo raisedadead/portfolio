a44e7a64be746be793774580b1764bb9cbad9d0a
Mrugesh Mohapatra <noreply@mrugesh.dev>
2025-06-14 11:03:35 +0530

    chore: cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppType } from 'next/dist/shared/lib/utils';
 import '@/styles/globals.css';
-import { fontSans, fontMono } from '@/components/fonts';
 import ConsentBanner from '@/components/consent-banner';
 import Script from 'next/script';
 import { useState, useEffect } from 'react';
